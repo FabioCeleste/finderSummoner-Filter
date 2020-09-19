ff41f0bb43c6c747d2dd15c2f41b65f2a3a6b500
FabioCeleste <36655978+FabioCeleste@users.noreply.github.com>
2020-09-18 23:59:43 -0300

    spells

--- index.js
+++ index.js
@@ -1,30 +1,60 @@
 var glob = require("glob");
 var fs = require("fs");
 
-
-const champArray = [];
-glob("./champs3/*.json", function (err, files) {
-  files.forEach((element) => {
-    fs.readFile(element, "utf8", function (err, data) {
-      var obj = JSON.parse(data);
-      obj = obj.data;
-      obj = obj[Object.keys(obj)[0]];
-      const a= {
-          id: obj.id,
-          key: obj.key,
-          image: {
-            full: obj.image.full,
-            sprite: obj.image.sprite,
-          },
-      }
-      champArray.push(a)      
+function champs(){
+  const champArray = [];
+  glob("./champs3/*.json", function (err, files) {
+    files.forEach((element) => {
+      fs.readFile(element, "utf8", function (err, data) {
+        var obj = JSON.parse(data);
+        obj = obj.data;
+        obj = obj[Object.keys(obj)[0]];
+        const a= {
+            id: obj.id,
+            key: obj.key,
+            image: {
+              full: obj.image.full,
+              sprite: obj.image.sprite,
+            },
+        }
+        champArray.push(a)      
+      });
     });
-  });
-})
-setTimeout(function(){ 
-    const json = JSON.stringify(champArray)
-    fs.writeFile('champs3.json', json, (err) => {
-        if(err) return console.log(err)
-        console.log('data saved')
-    } )  
- }, 3000);
+  })
+  setTimeout(function(){ 
+      const json = JSON.stringify(champArray)
+      fs.writeFile('champs3.json', json, (err) => {
+          if(err) return console.log(err)
+          console.log('data saved')
+      } )  
+   }, 3000);
+}
+
+function spells(){
+  const saveJson = {}
+  const summonerJson = fs.readFile('summoner.json', (err, data) => {
+    const obj = JSON.parse(data)
+    dataSpell = obj.data
+    const keys = Object.keys(obj.data)
+    keys.map(spell => {
+      const spellData = dataSpell[spell]
+      saveJson[spellData.key] = {
+        name: spellData.name,
+        image: {
+          full: spellData.image.full,
+          sprite: spellData.image.sprite
+        }
+      }      
+    })
+    const jsonString = JSON.stringify(saveJson)
+    fs.writeFile('spells.json', jsonString, (err) => {
+      if(err) return console.log(err)
+      console.log('data saved')
+  } )
+    
+  })
+  
+}
+
+spells()
+
